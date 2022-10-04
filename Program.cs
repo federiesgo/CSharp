067f436cbd3078675310e7878dcec3cb90c1e451
Federico Riesgo <federico.riesgo@ibisdev.tech>
2022-10-04 20:53:51 -0300

    Agregando hora actual

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿Console.WriteLine("Introduce tu nombre");
 
-string Nombre = Console.ReadLine();
+string Nombre = Console.ReadLine(); 
 
 Console.WriteLine("Hola " + Nombre + ", Bienvenido.");
+Console.WriteLine("La hora actual es: " + DateTime.Now.ToString("h:mm:ss tt"));
