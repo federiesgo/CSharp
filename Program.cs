8ee4c23bf89de3bfe6c05a626dbadc3cc466ab00
Federico Riesgo <federico.riesgo@ibisdev.tech>
2022-10-04 23:42:48 -0300

    Ejercicios clase 1

--- Program.cs
+++ Program.cs
@@ -1,7 +1,37 @@
-﻿Console.WriteLine("Introduce tu nombre");
+﻿//EJERCICIO 1
 
+/*Console.WriteLine("Ingrese su Nombre");
 string Nombre = Console.ReadLine();
-string HoraActual = DateTime.Now.ToString("h:mm:ss tt");
+Console.WriteLine("Ingrese su Apellido");
+string Apellido = Console.ReadLine();
+Console.WriteLine("Ingrese su Edad");
+string EdadStr = Console.ReadLine();
+int Edad = Int32.Parse(EdadStr);
+Console.WriteLine("Sabe programar?");
+string SabeProgramar = Console.ReadLine();
 
-Console.WriteLine($"Hola {Nombre}, Bienvenido.");
-Console.WriteLine($"La hora actual es: {HoraActual}");
+Console.WriteLine($"Hola {Nombre} {Apellido}. Usted tiene {Edad} años y {SabeProgramar} sabe programar");
+*/
+
+//EJERCICIO 2
+/*
+object Coche;
+int Puertas;
+int Ruedas;
+string Marca;
+Boolean VtvVigente;
+
+object Mesa;
+double peso;
+double largo;
+string material;
+string color;
+*/
+
+//EJERCICIO 3
+/*
+1) (num >= 18);
+2) (==);
+3) (true && true); 
+4) (true || false);
+*/
