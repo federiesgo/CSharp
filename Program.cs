86d6aefdd6a4c0bad7a92310942fd03867d86f2e
Federico Riesgo <federico.riesgo@ibisdev.tech>
2022-10-04 21:13:05 -0300

    Cambiando la hora actual a una variable

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.WriteLine("Introduce tu nombre");
 
-string Nombre = Console.ReadLine(); 
+string Nombre = Console.ReadLine();
+string HoraActual = DateTime.Now.ToString("h:mm:ss tt");
 
 Console.WriteLine("Hola " + Nombre + ", Bienvenido.");
-Console.WriteLine("La hora actual es: " + DateTime.Now.ToString("h:mm:ss tt"));
+Console.WriteLine("La hora actual es: " + HoraActual);
