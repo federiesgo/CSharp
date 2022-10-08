1b6ea57eed5cae322e53476a63a89fbefd0efafd
Federico Riesgo <federico.riesgo@ibisdev.tech>
2022-10-07 22:39:01 -0300

    inheritance, polymorphism

---
+++ Persona.cs
@@ -0,0 +1,22 @@
+﻿public class Persona
+{
+    public string nombre;
+    public int edad;
+    public string nacionalidad;
+    public string profesion;
+    public bool trabaja;
+
+    public Persona (string nombre, int edad, string nacionalidad, string profesion, bool trabaja)
+    {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.nacionalidad = nacionalidad;
+        this.profesion = profesion;
+        this.trabaja = trabaja;
+    }
+
+    public virtual void consultarEdad()
+    {
+        Console.Write($"La persona tiene {edad} años y es {profesion}");
+    }
+}
